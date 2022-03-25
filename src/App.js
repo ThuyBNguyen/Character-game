f6b0a7ded9592475657a565a35d15b9f6cd00eba
Nha Phuong <nhaphuong5994@gmail.com>
2022-03-25 22:51:46 +0700

    add-homepage

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+        <div>
+        <h1>CHARACTER</h1>
+        <h2>CUSTOMIZATION</h2>
+        </div>
+        <div>{Avatar}</div>
+        <div>
+            <div>{bodyList}</div>
+            <div>{eyesList}</div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default App
